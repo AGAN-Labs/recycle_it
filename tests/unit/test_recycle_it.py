736654fb2197d41ccaaa1c4a435d207df8e7c0ef
noah-tocyen <noah@tocyen.org>
2021-05-28 17:43:40 -0400

    Added Django Project Files. Added Homepage App. Functional test now passes. HomePage Unit test also passes.

--- tests/unit/test_recycle_it.py
+++ tests/unit/test_recycle_it.py
@@ -8,9 +8,6 @@ class RecycleitTestCase(unittest.TestCase):
         if 'darwin' in platform.platform().lower():
             print('Using Mac OS X settings!')
 
-    def test_tocyen_online_has_title(self):
-        self.fail("Failed")
-
 
 if __name__ == '__main__':
     unittest.main()
