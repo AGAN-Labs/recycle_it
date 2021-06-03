4df95e8a36ad6418b08978eaf088f3410d9b0ae3
Derek <dckirwan07@gmail.com>
2021-06-03 12:07:47 -0400

    Updated functional_tests.py user story for sending images

--- tests/functional/functional_tests.py
+++ tests/functional/functional_tests.py
@@ -3,6 +3,7 @@ import platform
 import pathlib
 
 from selenium import webdriver
+import recycle_it.config as recycle_it_config
 
 class RecycleitFunctionalTest(unittest.TestCase):
 
@@ -23,9 +24,17 @@ class RecycleitFunctionalTest(unittest.TestCase):
 
         # We notice the page title and header mention the company name
         self.assertIn('Recycle It', self.browser.title)
-        #self.fail('Finish the test!')
 
-        # We see the login button
+        # We want to upload an image of an object to see if it is recyclable
+        # We see a prompt asking us to take or upload an image
+
+        # We know the endpoint for sending images
+        image_endpoint = recycle_it_config.image_endpoint
+        self.assertIn("/images", image_endpoint)
+
+        # We attempt to POST the image to the image endpoint
+
+        # We see a button for uploading a selected image
 
 
 if __name__ == '__main__':
