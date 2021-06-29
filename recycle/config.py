6cebc815a275982b9defac80edb45f47b99fcd33
Guy Monahan <guymonahan@gmail.com>
2021-06-29 12:18:56 -0400

    added to config more parameters for dataloaders into run

--- recycle/config.py
+++ recycle/config.py
@@ -8,3 +8,6 @@ data_dir = Path(__file__).parent.parent.joinpath('data/garbage_classification/')
 data_model_path = Path(__file__).parent.parent.joinpath('data/models/')
 num_epochs = 8
 learning_rate = 5.5e-5
+shuffle_training = True
+pin_memory = True
+batch_size = 32
